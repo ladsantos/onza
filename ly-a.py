ae1ced6dd63f02fdf7257fbd0096bc4f05c68332
Leonardo dos Santos <laugustogs@gmail.com>
2017-10-06 11:06:03 +0200

    Implemented the total optical depth computation

--- ly-a.py
+++ ly-a.py
@@ -1,24 +1,35 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import astropy.units as u
-import astropy.constants as c
 
 
 # Lyman-alpha absorption class
 class Absorption(object):
     """
 
+    Args:
+        grid: Image of transit
+        positions: Positions of particles
+        velocities: Velocities of particles
+        cell_size: Size of cell
+        stellar_radius: Stellar radius
+        res_element: Resolution element of the instrument, in velocity units
+        vel_range: Range of velocities of the spectrum (not to be confused with
+            the velocities of particles!)
     """
-    def __init__(self, grid, positions, velocities, supergrid_size=10,
-                 stellar_radius=1 * u.solRad, res_element=20 * u.km / u.s):
+    def __init__(self, grid, positions, velocities, cell_size=10,
+                 stellar_radius=1 * u.solRad, res_element=20 * u.km / u.s,
+                 vel_range=[-300, 300] * u.km / u.s):
         self.grid = grid
         self.g_size = len(grid)
         self.pos_starcentric = positions
         self.vel_starcentric = velocities
         self.res_element = res_element
-        self.sg_size = supergrid_size
-        self.sg_step = self.g_size // supergrid_size
-        self.sg_last_step = self.sg_step + self.g_size % supergrid_size
+        self.c_size = cell_size
+        self.c_step = self.g_size // cell_size
+        self.c_last_step = self.c_step + self.g_size % cell_size
+        self.vel_bins = np.arange(vel_range[0] - res_element / 2,
+                                  vel_range[1] + res_element * 3 / 2,
+                                  res_element)
 
         # Physical constants
         self.damp_const = 6.27E8 / u.s
@@ -42,9 +53,9 @@ class Absorption(object):
         """
 
         Args:
-            vel_i:
-            vel_j:
-            num_e:
+            vel_i: Reference velocity
+            vel_j: Current velocity
+            num_e: Number of particles with current velocity
 
         Returns:
             tau:
@@ -66,6 +77,53 @@ class Absorption(object):
         tau = self.sigma_v_0 * num_e * self.lambda_0 / self.res_element
         return tau
 
-    # The total absorption
-    def tau_h(self):
-        pass
+    # Compute the number of hydrogen particles inside a cell within a given
+    # velocity range in the z-axis
+    def num_particles(self, cell_indexes, velocity_range):
+        """
+
+        Args:
+            cell_indexes: In the form [[i0, j0], [i1, j1]]
+            velocity_range: In the form [v0, v1]
+
+        Returns:
+
+        """
+        i0, j0, i1, j1 = cell_indexes[0][0], cell_indexes[0][1], \
+                         cell_indexes[1][0], cell_indexes[1][1]
+        v0, v1 = velocity_range[0], velocity_range[1]
+        inds = np.where(i0 < self.pos[:, 0] < i1 and j0 < self.pos[:, 1] < j1
+                        and v0 < self.vel[:, 2] < v1)
+        n_c_v = len(inds)
+        return n_c_v
+
+    # The total optical depth
+    def tau(self, cell_indexes, velocity_bin):
+        """
+
+        Args:
+            cell_indexes: In the form [[i0, j0], [i1, j1]]
+            velocity_bin: Index of the bin (left side) of the desired velocity
+                range self.vel_bins
+
+        Returns:
+
+        """
+        k = velocity_bin
+        tau_broad = []
+        ref_vel = (self.vel_bins[k] + self.vel_bins[k + 1]) / 2
+        ref_vel_range = [self.vel_bins[k], self.vel_bins[k + 1]]
+        for i in range(len(self.vel_bins) - 1):
+            cur_vel = self.vel_bins[i] + self.res_element / 2
+            cur_vel_range = [self.vel_bins[i], self.vel_bins[i + 1]]
+            t0 = self.sigma_v_0 * self.num_particles(cell_indexes,
+                                                     cur_vel_range)
+            t1 = self.damp_const / 4 / np.pi ** 2 * self.lambda_0 ** 2
+            t2 = (cur_vel - ref_vel) ** (-2)
+            tau_broad.append(t0 * t1 * t2)
+        tau_broad = np.array(tau_broad)
+        tau_broad = np.delete(tau_broad, k, 0)
+        ref_num_e = self.num_particles(cell_indexes, ref_vel_range)
+        tau = self.tau_line(ref_num_e) + np.sum(tau_broad)
+
+        return tau
