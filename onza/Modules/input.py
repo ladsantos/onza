Computation of absorption profiles are now correct

onza/Modules/input.py
@@ -8,6 +8,7 @@ This module computes input data for the lyman_alpha module.
 from __future__ import (division, print_function, absolute_import,
                         unicode_literals)
 import numpy as np
+from itertools import product
 
 
 __all__ = ["ParticleEnsemble", "DensityMap"]
@@ -25,7 +26,8 @@ class _OnzaInput(object):
         cell_area:
 
     """
-    def __init__(self, cell_bin, vel_bin, cell_area=None):
+    def __init__(self, cell_bin, vel_bin, cell_area=None,
+                 px_physical_area=40680159.61):
 
         self.cell_bin = cell_bin
         self.vel_bin = vel_bin
@@ -42,7 +44,7 @@ class _OnzaInput(object):
                     area.append((self.cell_bin[i + 1] - self.cell_bin[i]) *
                                 (self.cell_bin[j + 1] - self.cell_bin[j]))
                 self.cell_area.append(area)
-            self.cell_area = np.array(self.cell_area)
+            self.cell_area = np.array(self.cell_area) * px_physical_area
 
         # Compute the `doppler_shift` array, which consists on the values of the
         # Doppler shift away from the line center computed as the mean of two
@@ -127,6 +129,26 @@ class DensityMap(_OnzaInput):
         self.map = density_map
         self.vel_dist = vel_dist
 
+        # Computing the coarse map (cells instead of pixels)
+        cells = np.arange(len(cell_bin) - 1)
+        self.map_coarse = np.zeros([len(cells), len(cells)], float)
+
+        for i, j in product(cells, cells):
+
+            # The last column and lines have to be added manually
+            if i == cells[-1]:
+                self.map_coarse[i, j] = np.sum(
+                    self.map[cell_bin[i]:cell_bin[i + 1] + 1,
+                             cell_bin[j]:cell_bin[j + 1]])
+            elif j == cells[-1]:
+                self.map_coarse[i, j] = np.sum(
+                    self.map[cell_bin[i]:cell_bin[i + 1],
+                             cell_bin[j]:cell_bin[j + 1] + 1])
+            else:
+                self.map_coarse[i, j] = np.sum(
+                    self.map[cell_bin[i]:cell_bin[i + 1],
+                             cell_bin[j]:cell_bin[j + 1]])
+
         # Computing the density cube
-        self.density = np.array([vk * self.map * self.res_element
-                                 for vk in self.vel_dist])
+        self.density = np.array([vk * self.map_coarse * self.res_element
+                                 for vk in self.vel_dist]) / self.cell_area

onza/Modules/transit.py
@@ -101,11 +101,13 @@ class Grid(object):
         raise NotImplementedError('This method is not implemented yet.')
 
     # Compute a cell grid
-    def draw_cells(self, cell_size=10):
+    def draw_cells(self, cell_size=10, px_physical_area=40680159.61):
         """
 
         Args:
             cell_size:
+
+            px_physical_area (`float`): In km ** 2
 
         Returns:
 
@@ -123,7 +125,7 @@ class Grid(object):
                 area.append((self.cell_bin[i + 1] - self.cell_bin[i]) *
                             (self.cell_bin[j + 1] - self.cell_bin[j]))
             self.cell_area.append(area)
-        self.cell_area = np.array(self.cell_area)
+        self.cell_area = np.array(self.cell_area) * px_physical_area
 
 
 # Test module

onza/Tests/test_lyman_alpha.py
@@ -2,14 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from onza.Modules import transit, input, lyman_alpha
 
+# Generate a stellar disk
+grid = transit.Grid(size=221)
+grid.draw_star([110, 110], 109)
+grid.draw_planet([110, 110], 1)
+grid.draw_cells(20)
+
+cell_bin = grid.cell_bin
+cell_area = grid.cell_area
+vel_range = (-300, 300)
+res_element = 10
+vel_bin = np.arange(vel_range[0] - res_element / 2,
+                    vel_range[1] + res_element * 3 / 2,
+                    res_element)
+
 
 def test_particle():
-
-    # Generate a stellar disk
-    grid = transit.Grid(size=221)
-    grid.draw_star([110, 110], 109)
-    grid.draw_planet([110, 110], 1)
-    grid.draw_cells(10)
 
     # Generate positions and velocities with a Gaussian distribution
     n_particles = int(1E6)
@@ -17,15 +25,9 @@ def test_particle():
     part_vel = np.random.normal(loc=0, scale=30., size=(3, n_particles))
 
     # Compute the density cube
-    cell_bin = grid.cell_bin
-    cell_area = grid.cell_area
-    vel_range = (-300, 300)
-    res_element = 10
-    vel_bin = np.arange(vel_range[0] - res_element / 2,
-                        vel_range[1] + res_element * 3 / 2,
-                        res_element)
+
     cube = input.ParticleEnsemble(part_pos, part_vel, cell_bin, vel_bin,
-                                  cell_area, atoms_per_particle=1E22)
+                                  cell_area, atoms_per_particle=1E26)
 
     ab = lyman_alpha.Absorption(grid, cube)
     ab.compute_abs()
@@ -37,13 +39,40 @@ def test_particle():
     plt.ylabel(r'Flux')
     plt.legend()
     plt.tight_layout()
-    plt.savefig('../../dump/test_lyman_alpha.pdf')
+    plt.savefig('../../dump/test_particle.pdf')
 
 
 def test_map():
-    pass
+
+    # Import data
+    map = np.loadtxt('../../dump/model_earthRad-2d.dat.gz')
+
+    # The distribution of velocities
+    scale = 30.0
+    loc = 0.0
+    vels = []
+    for i in range(len(vel_bin) - 1):
+        vels.append((vel_bin[i] + vel_bin[i + 1]) / 2)
+    vels = np.array(vels)
+    vel_dist = 1 / scale / np.sqrt(2 * np.pi) * \
+        np.exp(-0.5 * ((vels - loc) / scale) ** 2)
+
+    # Finally compute the cube
+    cube = input.DensityMap(map, vel_bin, vel_dist, cell_bin, cell_area)
+
+    ab = lyman_alpha.Absorption(grid, cube)
+    ab.compute_abs()
+
+    plt.plot(ab.doppler_shift, ab.flux, label=r'$\sigma_{\mathrm{vel}}$ = '
+                                              r'30 km/s')
+
+    plt.xlabel(r'Doppler shift from Ly-$\alpha$ (km s$^{-1}$)')
+    plt.ylabel(r'Flux')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('../../dump/test_density.pdf')
 
 
 if __name__ == '__main__':
     test_particle()
-    # test_map()
+    test_map()