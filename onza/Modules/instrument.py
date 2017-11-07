1ff1770ab1247f5f9f57e916c306a597aab50eaf
leonardo <leonardo.dossantos@unige.ch>
2017-11-07 12:01:04 +0100

    Strings 'gauss' changed to 'gaussian'

--- onza/Modules/instrument.py
+++ onza/Modules/instrument.py
@@ -16,9 +16,17 @@ class HubbleSTIS(object):
     """
 
     """
-    def __init__(self, bandwidth, config=None, simulation_path=None):
+    def __init__(self, resolution, config=None, simulation_path=None):
+        """
+
+        Args:
+            resolution: Resolution of the band spectrum
+            config:
+            simulation_path:
+        """
+        # LSF = Line Spread Function
         self.config = config
-        self.bandwidth = bandwidth
+        self.resolution = resolution
         self.sim_path = simulation_path
 
         # Instantiating useful global variables
@@ -33,64 +41,61 @@ class HubbleSTIS(object):
             self.LSF_core = None
             self.LSF_wing = None
             self.hrange_LSF = None
-            self.dw_inst = None
+            self.instrument_resolution = None
 
         # HD189_2010 configuration
         elif self.config == 'STIS_HI_HD189_2010':
-            # XXX XXX XXX XXX XXX
-            # TODO: I need to know exactly what are these parameters
-            # XXX XXX XXX XXX XXX
             self.response_mode = 'LSF'
-            self.kernel_mode = 'double_gauss'
+            self.kernel_mode = 'double_gaussian'
             self.coeff_LSF = 0.88
             self.LSF_core = 0.0416
             self.LSF_wing = 0.2027
             self.hrange_LSF = 0.626343
-            self.dw_inst = 0.05335
+            self.instrument_resolution = 0.05335
 
         # HD189_2011 configuration
         elif self.config == 'STIS_HI_HD189_2011':
             self.resp_mode='LSF'
-            self.kernel_mode='double_gauss'
+            self.kernel_mode='double_gaussian'
             self.coeff_LSF=0.9456
             self.LSF_core=0.0665
             self.LSF_wing=0.2665
             self.hrange_LSF=0.754044
-            self.dw_inst=0.05337
+            self.instrument_resolution=0.05337
 
         # HD209 configuration
         elif self.config == 'STIS_HI_HD209':
             self.resp_mode = 'LSF'
-            self.kernel_mode = 'double_gauss'
+            self.kernel_mode = 'double_gaussian'
             self.coeff_LSF = 0.51785155
             self.LSF_core = 0.094208
             self.LSF_wing = 0.051881
             self.hrange_LSF = 0.33307602
-            self.dw_inst = 0.05302
+            self.instrument_resolution = 0.05302
 
         # GJ436 configuration 0
         elif self.config == 'GJ436_HI_0':
             self.resp_mode = 'LSF'
-            self.kernel_mode = 'single_gauss'
-            self.dw_LSF = 0.07652675
+            self.kernel_mode = 'single_gaussian'
+            self.fwhm_LSF = 0.07652675
             self.hrange_LSF = 0.284463523
-            self.dw_inst = 0.0530366854
+            self.instrument_resolution = 0.0530366854
 
         # GJ436 configuration 2
         elif self.config == 'GJ436_HI_2':
             self.resp_mode = 'LSF'
-            self.kernel_mode = 'single_gauss'
-            self.dw_LSF = 0.05174745
+            self.kernel_mode = 'single_gaussian'
+            self.fwhm_LSF = 0.05174745
             self.hrange_LSF = 0.192322867
-            self.dw_inst = 0.0533312197
+            self.instrument_resolution = 0.0533312197
 
         # GJ436 configuration 3
         elif self.config == 'GJ436_HI_3':
             self.resp_mode = 'LSF'
-            self.kernel_mode = 'single_gauss'
-            self.dw_LSF = 0.06670499
+            self.kernel_mode = 'single_gaussian'
+            self.fwhm_LSF = 0.06670499
             self.hrange_LSF = 0.247939299
-            self.dw_inst = 0.05333425
+            self.instrument_resolution = 0.05333425
 
         # Kepler-444 configuration
         elif self.config == 'Kepler444_HI':
@@ -102,8 +107,8 @@ class HubbleSTIS(object):
         # HD977658 configuration
         elif self.config == 'HD97658_HI_23':
             self.resp_mode = 'LSF'
-            self.kernel_mode = 'double_gauss'
-            self.dw_inst = 0.0533
+            self.kernel_mode = 'double_gaussian'
+            self.instrument_resolution = 0.0533
             self.coeff_LSF = 0.933
             self.LSF_core = 0.0533
             self.LSF_wing = 0.3069547
@@ -112,8 +117,8 @@ class HubbleSTIS(object):
         # TRAPPIST1 configuration
         elif self.config == 'TRAPPIST1_HI':
             self.resp_mode = 'LSF'
-            self.kernel_mode = 'double_gauss'
-            self.dw_inst = 0.0533
+            self.kernel_mode = 'double_gaussian'
+            self.instrument_resolution = 0.0533
             self.coeff_LSF = 0.935946
             self.LSF_core = 0.05174577
             self.LSF_wing = 0.245484
@@ -134,18 +139,18 @@ class HubbleSTIS(object):
         """
         # Half number of pixels in the kernel table at the resolution of the
         # band spectrum
-        half_number = int(self.hrange_LSF / self.bandwidth) + 1
+        half_number = int(self.hrange_LSF / self.resolution) + 1
 
         # Centered spectral table with same pixel widths as the band spectrum
         # the kernel is associated to
         n_kernel = 2 * half_number + 1
-        self.wavelength = self.bandwidth * (np.arange(n_kernel) -
-                                            (n_kernel - 1) / 2)
+        self.wavelength = self.resolution * (np.arange(n_kernel) -
+                                             (n_kernel - 1) / 2)
 
         # Single Gaussian
-        if self.kernel_mode == 'single_gauss':
+        if self.kernel_mode == 'single_gaussian':
             self.kernel = np.exp(-np.power(self.wavelength /
-                                           (np.sqrt(2) * self.dw_LSF), 2))
+                                           (np.sqrt(2) * self.fwhm_LSF), 2))
 
         # Double Gaussian
         elif self.kernel_mode == 'double_gaussian':
